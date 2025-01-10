79da3c7e786c918039cc44ce019597b4a0f607dc
horseywin <horsey2019@duck.com>
2025-01-10 17:17:22 +1000

    Made the js script more efficient

--- script.js
+++ script.js
@@ -7,32 +7,30 @@ console.log("added button")
 
 */
 
-function addPopUp(title, description, imageURL) {
+
+//tetris
+function tetrisPopup() {
+document.getElementById("tetris-game").addEventListener("click", () => {
+    console.log("spawning popup")
+    addPopUp("Tetris game", "This project is something I came up with as a challenge. My teacher was speaking of an grade 10 assessment on building a Tetris game. The only language I could do it with was Python. So here I am. Using Godot or another language like javascript would have been an easier language. Python is not a language of games or UI nevertheless and Tetris game! I used coloured emojis like {🟥 🟦 🟨} to make a decent enough appeal. As of 2025. THIS IS THE HARDEST PROJECT I HAVE EVER DONE. Have a look at the code at the github page below!", "Tetris-Game-Coding.png", "https://github.com/horseywin/Tetris-Game")
+}
+)}
+
+tetrisPopup()
+
+function addPopUp(title, description, imageURL, link) {
     const BodyTag = document.getElementsByTagName("body")[0]
     BodyTag.innerHTML += `
     <div class="popup" style="background-image: url('${imageURL}');">
         <h1 class="popup-title">${title}</h1>
         <h3 class="popup-description">${description}</h3>
-        <a class="popup-description" href="https://github.com/horseywin/Tetris-Game">Github project here</a>
+        <a class="popup-description" href="${link}" style="text-decoration: underline;">Github project here</a>
     </div>
     <div class="popup-exit"></div>
     `
-    console.log("ADDED THE POPUP TO INNER HTML")
     document.getElementsByClassName("popup-exit")[0].addEventListener("click", () => {
         document.getElementsByClassName("popup")[0].remove()
         document.getElementsByClassName("popup-exit")[0].remove()
     });
-    document.getElementById("tetris-game").addEventListener("click", () => {
-        console.log("spawning popup")
-        addPopUp("Tetris game", "This project is something I came up with as a challenge. My teacher was speaking of an upcoming assessment on building a Tetris game. He mentioned that it didn't matter how the students did it, as long as it was good. When I mean upcoming, I dont mean 2-5 months ahead. No. This assessment was 2 years into the future when my peers and I would have been more advanced at programming. The only language I could do it with was Python. So here I am. Using Godot or another language like javascript would have been an easier language. Python is not a language of games or UI nevertheless and Tetris game! I used coloured emojis like {🟥 🟦 🟨} to make a decent enough appeal. As of 2025. THIS IS THE HARDEST PROJECT I HAVE EVER DONE", "Tetris-Game-Coding.png")
-    }
-    )
+    tetrisPopup()
 }
-//tetris
-document.getElementById("tetris-game").addEventListener("click", () => {
-    console.log("spawning popup")
-    addPopUp("Tetris game", "This project is something I came up with as a challenge. My teacher was speaking of an upcoming assessment on building a Tetris game. He mentioned that it didn't matter how the students did it, as long as it was good. When I mean upcoming, I dont mean 2-5 months ahead. No. This assessment was 2 years into the future when my peers and I would have been more advanced at programming. The only language I could do it with was Python. So here I am. Using Godot or another language like javascript would have been an easier language. Python is not a language of games or UI nevertheless and Tetris game! I used coloured emojis like {🟥 🟦 🟨} to make a decent enough appeal. As of 2025. THIS IS THE HARDEST PROJECT I HAVE EVER DONE", "Tetris-Game-Coding.png")
-}
-)
-
-document.getElementById("tetris-game").addEventListener("click", () => console.log("clicked"))
